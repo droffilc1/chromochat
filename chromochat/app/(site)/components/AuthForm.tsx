a4d8543c7817a653d7277456678a24ec5087def9
El Houssain Souhail <sou.ductive@gmail.com>
2024-07-17 21:59:51 +0100

    Added sign up page with db and prisma setup for registration

---
+++ chromochat/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,169 @@
+'use client';
+
+import AuthSocialButton from './AuthSocialButton';
+import Input from '@/app/components/inputs/Input';
+import Button from '@/app/components/Button';
+
+import { useCallback, useState } from 'react';
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios';
+
+
+type Variant = 'SIGNIN' | 'SIGNUP';
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>('SIGNIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'SIGNIN') {
+            setVariant('SIGNUP');
+        } else {
+            setVariant('SIGNIN');
+        }
+    }, [variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+        if (variant === 'SIGNIN') {
+            // NextAuth signin logic
+        }
+        if (variant === 'SIGNUP') {
+            axios.post('/api/register', data)
+        }
+    }
+
+    const socialAction = (action: string) => {
+        setIsLoading(true);
+
+        // NextAuth social signin logic
+    }
+
+    return (
+        <div
+            className="
+            mt-8
+            sm:mx-auto
+            sm:w-full
+            sm:max-w-md"
+        >
+            <div
+                className="
+                bg-white
+                py-8
+                px-4
+                shadow
+                sm:rounded-lg
+                sm:px-10"
+            >
+                <form
+                    className="space-y-6"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    {variant === 'SIGNUP' && (
+                        <Input
+                            id="name"
+                            label="Name"
+                            register={register}
+                            errors={errors}
+                            disabled={isLoading}
+                        />
+                    )}
+                    <Input
+                        id="email"
+                        label="Email address"
+                        type="email"
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}
+                    />
+                    <Input
+                        id="password"
+                        label="Password"
+                        type="password"
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}
+                    />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >
+                            {variant === 'SIGNIN' ? 'Sign in' : 'Sign up'}
+                        </Button>
+                    </div>
+                </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div
+                            className="
+                            absolute
+                            inset-0
+                            flex
+                            items-center"
+                        >
+                            <div
+                                className="
+                                    w-full
+                                    border-t
+                                    border-gray-300"
+                                />
+                        </div>
+                        <div className="relative flex justify-center text-sm ">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton 
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialButton 
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+                </div>
+                <div className="
+                    flex
+                    gap-2
+                    justify-center
+                    text-sm
+                    mt-6
+                    px-2
+                    text-gray-500
+                ">
+                    <div>
+                        {variant === 'SIGNIN' ? 'New to ChromoChat?' : 'Already have an account?'}
+                    </div>
+                    <div
+                        onClick={toggleVariant}
+                        className="underline cursor-pointer"
+                    >
+                        {variant === 'SIGNIN' ? 'Create an account' : 'Sign in'}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AuthForm;
---
+++ chromochat/app/components/Button.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import clsx from 'clsx';
+
+interface ButtonProps {
+    type?: 'button' | 'submit' | 'reset' | undefined;
+    fullWidth?: boolean;
+    children?: React.ReactNode;
+    onClick?: () => void;
+    secondary?: boolean;
+    danger?: boolean;
+    disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    type,
+    fullWidth,
+    children,
+    onClick,
+    secondary,
+    danger,
+    disabled,
+}) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className={clsx(`
+                flex
+                justify-center
+                rounded-md
+                py-2
+                px-4
+                text-sm
+                font-semibold
+                foucs-visible:outline
+                foucs-visible:outline-2
+                foucs-visible:outline-offset-2
+            `,
+            disabled && "opacity-50 cursor-default",
+            fullWidth && "w-full",
+            secondary ? "text-gray-700 bg-gray-200" : "text-white bg-secondColor",
+            danger && "bg-red-600 hover:bg-red-700 focus-visible:outline-red-800",
+            !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-800",
+            )}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
