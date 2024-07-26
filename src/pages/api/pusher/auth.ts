6c98488a598412b8981405a30e9aaf4d9829de63
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-07-26 13:42:02 +0300

    fix: build errors

--- src/pages/api/pusher/auth.ts
+++ src/pages/api/pusher/auth.ts
@@ -2,10 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { getServerSession } from "next-auth";
 
 import { pusherServer } from "@/app/libs/pusher";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 
 export default async function handler(
-    request: NextApiRequest, 
+    request: NextApiRequest,
     response: NextApiResponse
 ) {
     const session = await getServerSession(request, response, authOptions);
